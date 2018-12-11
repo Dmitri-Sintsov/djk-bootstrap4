29576594402411f45f3f11246148edb90ed25c7a
Dmitriy Sintsov <questpc256@gmail.com>
2018-12-11 20:28:06 +0300

    LAYOUT_CLASSES for bs4.

---
+++ djk_ui/conf.py
@@ -0,0 +1,10 @@
+LAYOUT_CLASSES = {
+    '': {
+        'label': 'col-md-3',
+        'field': 'col-md-7',
+    },
+    'display': {
+        'label': 'w-25 table-light',
+        'field': 'w-100 table-primary',
+    },
+}
